41c94a1462dafa2809b7b0eb5c5c5c85228accf0
QueruelGab <QueruelGab@gmail.com>
2023-07-08 17:27:47 +0200

    Base scripts Unit / Battle State / Battle HUD

--- Assets/Scripts/BattleSystem.cs
+++ Assets/Scripts/BattleSystem.cs
@@ -18,14 +18,18 @@ public class BattleSystem : MonoBehaviour
     
     public Text dialogueText;
 
+    public BattleHUD playerHUS;
+    public BattleHUD enemyHUD;
+
+
     public BattleState state;
 
     void Start()
     {
         state = BattleState.START;
-        SetupBattle();
+        StartCoroutine(SetupBattle());
     }
-    void SetupBattle()
+    IEnumerator SetupBattle()
     {
         GameObject playerGo = Instantiate(playerPrefab, playerBattleStation);
         playerUnit = playerGo.GetComponent<Unit>();
@@ -34,5 +38,109 @@ public class BattleSystem : MonoBehaviour
         enemyUnit = enemyGo.GetComponent<Unit>();
 
         dialogueText.text = "A wild " + enemyUnit.unitName + " approaches ...";
+
+        playerHUD.setHUD(playerUnit);
+        enemyHUD.SetHUF(enemyUnit);
+
+        yield return new WaitForSeconds(2f);
+
+        state = BattleState.PLAYERTURN;
+        PlayerTurn();
     }
+
+    IEnumerator PlayerAttack()
+    {
+        //Damage the enemy
+        bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
+
+        enemyHUD.SetHP(enemyUnit.currentHP);
+        dialogueText.text = "the attack is succesful";
+
+        yield return new WaitForSeconds(2f);
+
+        //check if the enemy is dead
+        if(isDead)
+        {
+            //End the Battle
+            state = BattleState.WON;
+            EndBattle();
+        }
+        else
+        {    
+            //Enemy's turn
+            state = BattleState.ENEMYTURN;
+            StartCoroutine(EnemyTurn());
+        }   
+    }
+    IEnumerator PlayerHeal()
+    {
+        playerUnit.Heal(5);
+
+        playerHUD.SetHP(playerUnit.currentHP);
+        dialogueText.text = "You feel renewed strenght!";
+
+        yield return new WaitForSeconds(2f);
+
+        state = BattleState.ENEMYTURN;
+        StartCoroutine(EnemyTurn());
+           
+    }
+
+    IEnumerator EnemyTurn()
+    {
+        dialogueText.text = enemyUnit.unitName + " attacks!";
+
+        yield return new WaitForSeconds(1f);
+
+        bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
+
+        playerHUD.SetHP(playerUnit.currentHP);
+
+        yield return new WaitForSeconds(1f);
+
+        if (isDead)
+        {
+            state = BattleState.LOST;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.PLAYERTURN;
+            PlayerTurn();
+        }
+    }
+
+    void EndBattle()
+    {
+        if (state == BattleState.WON)
+        {
+            dialogueText.text = "You won the battle!";
+        }
+        elseif (state == BattleState.LOST)
+        {
+            dialogueText.text = "You were defeated...";
+        }
+    }
+
+    void PlayerTurn()
+    {
+        dialogueText.text = "Choose an action :";
+    }
+
+    void OnAttackButton()
+    {
+        if(state != BattleState.PLAYERTURN)
+            return;
+        
+        StartCoroutine(PlayerAttack());
+    }
+    void OnHealButton()
+    {
+        if(state != BattleState.PLAYERTURN)
+            return;
+        
+        StartCoroutine(PlayerHeal());
+    }
+
+
 }
